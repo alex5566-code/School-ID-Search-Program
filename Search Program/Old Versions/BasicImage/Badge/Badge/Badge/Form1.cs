7976289cbf29db8450f693ac87047cee0b6e8b29
alex5566-code <58333672+alex5566-code@users.noreply.github.com>
2025-06-23 09:48:31 -0400

    Add files via upload

---
+++ Search Program/Old Versions/BasicImage/Badge/Badge/Badge/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Badge
+{
+    public partial class Form1 : Form
+    {
+        Image file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "JPG (*.JPG)|*.jpg";
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                file = Image.FromFile(f.FileName);
+                pictureBox1.Image = file;
+                // Image.FromFile("@C:\\Users\\gbertollini\\Desktop\\Badge\\123456789.jpg");
+            }
+        }
+    }
+}
